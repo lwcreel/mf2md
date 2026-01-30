2840f9ea4e5d7b6c044eb74c58ad76d89d6fb78c
Landon <landon.creel@protonmail.com>
2026-01-30 15:21:52 -0800

    Added Serde to Struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use csv::Reader;
+use serde::Deserialize;
+use std::error::Error;
+
+#[derive(Debug, serde::Deserialize, PartialEq, Eq)]
 struct Workout {
+    #[serde(rename = "Weight")]
     weight: u16,
+    #[serde(skip_deserializing)]
     num_sets: u8,
+    #[serde(rename = "RIR")]
     rir: u8,
+    #[serde(rename = "Date")]
     date: String,
+    #[serde(rename = "Exercise")]
     exercise: String,
 }
 
