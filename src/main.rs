aea914e0866a89b6250a2533925c46acce2b7bcf
Landon <landon.creel@protonmail.com>
2026-01-30 15:42:43 -0800

    Adding Read Function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use csv::Reader;
 use serde::Deserialize;
-use std::error::Error;
+use std::{error::Error, fs::File, process};
 
 #[derive(Debug, serde::Deserialize, PartialEq, Eq)]
-struct Workout {
+struct ExerciseRecord {
     #[serde(rename = "Weight")]
     weight: u16,
     #[serde(skip_deserializing)]
@@ -16,6 +16,20 @@ struct Workout {
     exercise: String,
 }
 
+fn read_mf_csv() -> Result<(), Box<dyn Error>> {
+    let mut file = File::open("resources/sample.csv")?;
+    let mut rdr = csv::Reader::from_reader(file);
+
+    for result in rdr.deserialize() {
+        let exercise_record: ExerciseRecord = result?;
+        println!("{:?}", exercise_record);
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = read_mf_csv() {
+        println!("Error Running Example: {}", err);
+        process::exit(1);
+    }
 }
